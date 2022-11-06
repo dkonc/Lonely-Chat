912295052d8af30c2613ccc10f8c2d1b422792ad
dkonc <davidkonc@hotmail.com>
2022-11-06 19:33:56 +0100

    Messages

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import Messages from './Messages'
+import Input from './Input'
 const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className="chatInfo">
+        <span> Ana</span>
+        <div className="chatIcons">
+        </div>
+      </div>
+      <Messages/>
+      <Input/>
+    </div>
   )
 }
 
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
   return (
-    <div>Message</div>
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/1200px-Lion_waiting_in_Namibia.jpg" alt="" />
+        <span>Just now</span>      
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        { <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Lion_waiting_in_Namibia.jpg/1200px-Lion_waiting_in_Namibia.jpg" alt="" /> }
+      </div>
+    </div>
   )
 }
 
